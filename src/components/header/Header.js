2e9e68f8a1daa4a6f25f5e52db114ac3255d8baf
Escanour <anouaressaid60@gmail.com>
2022-09-08 17:21:30 +0100

    added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,57 @@
-import React from "react"
-import "./Header.css"
-import { Link } from "react-router-dom"
+import React, { Component } from "react";
+import "./Header.css";
+// import { Link } from "react-router-dom"
+import { ReactComponent as ReactLogo } from './image/MovieKech-1.svg';
+import { render } from "@testing-library/react";
+import { MenuData } from "./MenuData";
 
-const Header = () => {
-    return (
-        <div className="header">
-            <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
-                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
-                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
-                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
-            </div>
-        </div>
-    )
+
+class Header extends Component {
+    state = {clicked: false};
+    handleClick =()=>{
+        this.setState({...this.state, clicked: !this.state.clicked})
+    }
+    
+    render(){
+        console.log(this.state.clicked)
+        return (
+            <nav className="NavbarItems">
+                <ReactLogo />
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                    {MenuData.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a href={item.url} className={item.cName}> <i className={item.icon}></i>{item.title}</a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+        )
+    }
 }
 
 export default Header
+
+
+
+
+
+
+// cons  t Header = () => {
+//     return (
+//         <div className="header">
+//             <div className="headerLeft">
+//                 <ReactLogo />
+//             </div>
+//                 <navbar>
+//                     <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+//                     <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+//                     <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+//                 </navbar>
+//         </div>
+//     )
+// }
---
+++ src/components/header/MenuData.js
@@ -0,0 +1,37 @@
+export const MenuData = [
+    {
+        title: "Home",
+        url:"/",
+        cName:"nav-links",
+        icon:"fa-solid fa-house"
+    },
+    {
+        title: "Movies",
+        url:"",
+        cName:"nav-links",
+        icon:"fa-solid fa-film"
+    },
+    {
+        title: "series",
+        url:"",
+        cName:"nav-links",
+        icon:"fa-solid fa-tv"
+    },  {
+        title: "popular",
+        url:"/movies/popular",
+        cName:"nav-links",
+        icon:"fa-solid fa-fire"
+    },
+    {
+        title: "top_rated",
+        url:"/movies/top_rated",
+        cName:"nav-links",
+        icon:"fa-solid fa-star"
+    },
+    {
+        title: "upcoming",
+        url:"/movies/upcoming",
+        cName:"nav-links",
+        icon:"fa-solid fa-calendar-days"
+    },
+]
